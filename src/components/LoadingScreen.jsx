2767d093d075eeffa655457cc09802d51f5e2cae
kimm_leang <johnkimleang29@gmail.com>
2025-03-10 23:27:41 +0700

    porfolio website

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<Portfolio Website />";
+  const fullText = "<Portfolio/>";
 
   useEffect(() => {
     let index = 0;
