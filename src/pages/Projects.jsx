918d5b8d1614ae3793f6c5f1739d136916b5d116
kimm_leang <johnkimleang29@gmail.com>
2025-03-13 11:50:33 +0700

    add more image and projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,7 +14,7 @@ export const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <img src="/hr-project.jpg" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
+              <img src="/hrms.png" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
               <h3 className="text-xl font-bold mb-2">Human Resource System</h3>
               <p className="text-gray-400 mb-4">
                 HR management system with Attendance tracking, CRM and Payroll.
@@ -49,7 +49,7 @@ export const Projects = () => {
               transition-all
             "
             >
-              <img src="/doccare-project.jpg" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
+              <img src="/booking-app.png" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
               <h3 className="text-xl font-bold mb-2">Booking Appointment App</h3>
               <p className="text-gray-400 mb-4">
                 Mobile app for patient booking appointments with doctors, clinics and discuss about illness.
@@ -88,7 +88,124 @@ export const Projects = () => {
               transition-all
             "
             >
-              <img src="/E-commerce-project.jpg" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
+              <img src="/inventory.png" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
+              <h3 className="text-xl font-bold mb-2">Stock Management System</h3>
+              <p className="text-gray-400 mb-4">
+                Managing the stock, purchase product, supplier, customer and generate report.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React.js", "Tailwind", "Laravel", "google auth", "MySQL"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  Demo →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <img src="/job-seeking.jpg" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
+              <h3 className="text-xl font-bold mb-2">Scholarship and Job Seeking Website</h3>
+              <p className="text-gray-400 mb-4">
+                Allow student and employee to apply for scholarship and job, and company to post job.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Python", "Flask", "React", "AWS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  Demo →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <img src="/telegram-project.jpg" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
+              <h3 className="text-xl font-bold mb-2">Telegram Bot Notification</h3>
+              <p className="text-gray-400 mb-4">
+                Product added to stock, The notification will be send to stock Manager via telegram bot.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Next.js", "Laravel", "Bot Father",].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  Demo →
+                </a>
+              </div>
+            </div>
+            
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <img src="/e-com-web.jpg" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
               <h3 className="text-xl font-bold mb-2">E-Commerce Website</h3>
               <p className="text-gray-400 mb-4">
                 E-commerce website with a modern UI and product listing, cart and checkout features
@@ -121,84 +238,8 @@ export const Projects = () => {
               </div>
             </div>
 
-            <div
-              className="
-              glass p-6 rounded-xl border border-white/10 
-              hover:-translate-y-1 hover:border-blue-500/30
-              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
-              transition-all
-            "
-            >
-              <img src="/telegram-project.jpg" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
-              <h3 className="text-xl font-bold mb-2">Telegram Bot Notification</h3>
-              <p className="text-gray-400 mb-4">
-                Product added to stock, The notification will will be send using telegram bot.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["Next.js", "Laravel", "Bot Father",].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="
-                      bg-blue-500/10 text-blue-500 py-1 px-3 
-                      rounded-full text-sm
-                      transition
-                      hover:bg-blue-500/20 hover:-translate-y-0.5
-                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
-                    "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <div className="flex justify-between items-center ">
-                <a
-                  href="#"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                >
-                  Demo →
-                </a>
-              </div>
-            </div>
 
-            <div
-              className="
-              glass p-6 rounded-xl border border-white/10 
-              hover:-translate-y-1 hover:border-blue-500/30
-              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
-              transition-all
-            "
-            >
-              <img src="/inventory.png" alt="Project 1" className="w-full h-48 object-cover rounded-lg shadow-lg mb-2"/>
-              <h3 className="text-xl font-bold mb-2">Stock Management System</h3>
-              <p className="text-gray-400 mb-4">
-                Managing the stock, purchase product, supplier, customer and generate report.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["React.js", "Tailwind", "Laravel", "google"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="
-                      bg-blue-500/10 text-blue-500 py-1 px-3 
-                      rounded-full text-sm
-                      transition
-                      hover:bg-blue-500/20 hover:-translate-y-0.5
-                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
-                    "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <div className="flex justify-between items-center ">
-                <a
-                  href="#"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                >
-                  Demo →
-                </a>
-              </div>
-            </div>
-
+   
           </div>
         </div>
       </RevealOnScroll>
